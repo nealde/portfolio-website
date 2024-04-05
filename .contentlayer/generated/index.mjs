13580c678e202ae9c9886a49133702069ee19896
Sheila Goodman <goodman5@uw.edu>
2024-04-04 21:02:18 -0400

    updated gitignore, made some blog post stubs

--- .contentlayer/generated/index.mjs
+++ .contentlayer/generated/index.mjs
@@ -4,10 +4,10 @@ export { isType } from 'contentlayer/client'
 
 // NOTE During development Contentlayer imports from `.mjs` files to improve HMR speeds.
 // During (production) builds Contentlayer it imports from `.json` files to improve build performance.
-import allDocs from './Doc/_index.json' assert { type: 'json' }
+import { allPosts } from './Post/_index.mjs'
 
-export { allDocs }
+export { allPosts }
 
-export const allDocuments = [...allDocs]
+export const allDocuments = [...allPosts]
 
 
