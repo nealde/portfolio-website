d5426e3acda216b3c44ac7461efc338129d67d9c
neal <nealde@uw.edu>
2024-04-06 15:40:42 -0400

    made the compiler happy

--- components/github.tsx
+++ components/github.tsx
@@ -4,7 +4,7 @@ import GitHubCalendar from "react-github-calendar";
 
 export const Github = () => {
   return (
-    <div className="block justify-center items-center text-center text-white w-full">
+    <div className="mb-10 block justify-center items-center text-center text-white w-full">
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
         Days I <strong className="purple">Code</strong>
       </h1>
@@ -13,9 +13,9 @@ export const Github = () => {
           username="nealde"
           blockSize={15}
           blockMargin={5}
-          color="#c084f5"
+          // color={"#c084f5"}
           fontSize={16}
-          padding={20}
+          // padding={20}
           // className="justify-center"
         />
       </center>
--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,13 +1,16 @@
 import * as React from "react"
 import Image from "next/image"
 import { useMDXComponent } from "next-contentlayer/hooks"
+import type { MDXComponents } from 'mdx/types'
+// import { MDXProvider, useMDXComponents, MDXComponents } from '@mdx-js/react'
+
 
 import { cn } from "@/lib/utils"
 import { Callout } from "@/components/callout"
 import { MdxCard } from "@/components/mdx-card"
 
-const components = {
-  h1: ({ className, ...props }) => (
+const components: MDXComponents = {
+  h1: ({ className = "", ...props } : {className?: string}) => (
     <h1
       className={cn(
         "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
@@ -16,7 +19,7 @@ const components = {
       {...props}
     />
   ),
-  h2: ({ className, ...props }) => (
+  h2: ({ className, ...props } : {className?: string}) => (
     <h2
       className={cn(
         "mt-10 scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0",
@@ -25,7 +28,7 @@ const components = {
       {...props}
     />
   ),
-  h3: ({ className, ...props }) => (
+  h3: ({ className, ...props } : {className?: string}) => (
     <h3
       className={cn(
         "mt-8 scroll-m-20 text-2xl font-semibold tracking-tight",
@@ -34,7 +37,7 @@ const components = {
       {...props}
     />
   ),
-  h4: ({ className, ...props }) => (
+  h4: ({ className, ...props } : {className?: string}) => (
     <h4
       className={cn(
         "mt-8 scroll-m-20 text-xl font-semibold tracking-tight",
@@ -43,7 +46,7 @@ const components = {
       {...props}
     />
   ),
-  h5: ({ className, ...props }) => (
+  h5: ({ className, ...props } : {className?: string}) => (
     <h5
       className={cn(
         "mt-8 scroll-m-20 text-lg font-semibold tracking-tight",
@@ -52,7 +55,7 @@ const components = {
       {...props}
     />
   ),
-  h6: ({ className, ...props }) => (
+  h6: ({ className, ...props } : {className?: string}) => (
     <h6
       className={cn(
         "mt-8 scroll-m-20 text-base font-semibold tracking-tight",
@@ -61,28 +64,34 @@ const components = {
       {...props}
     />
   ),
-  a: ({ className, ...props }) => (
-    <a
-      className={cn("font-medium underline underline-offset-4", className)}
-      {...props}
-    />
-  ),
-  p: ({ className, ...props }) => (
+  // a: ({ className, ...props } : {className: string}) => (
+  //   <a
+  //     className={cn("font-medium underline underline-offset-4", className)}
+  //     {...props}
+  //   />
+  // ),
+  a: ({ className, ...props }: { className?: string }) => (
+        <a
+          className={cn("font-medium underline underline-offset-4", className || "")}
+          {...props}
+        />
+      ),
+  p: ({ className, ...props } : {className?: string}) => (
     <p
       className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
       {...props}
     />
   ),
-  ul: ({ className, ...props }) => (
+  ul: ({ className, ...props } : {className?: string}) => (
     <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
   ),
-  ol: ({ className, ...props }) => (
+  ol: ({ className, ...props } : {className?: string}) => (
     <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
   ),
-  li: ({ className, ...props }) => (
+  li: ({ className, ...props } : {className?: string}) => (
     <li className={cn("mt-2", className)} {...props} />
   ),
-  blockquote: ({ className, ...props }) => (
+  blockquote: ({ className, ...props } : {className?: string}) => (
     <blockquote
       className={cn(
         "mt-6 border-l-2 pl-6 italic [&>*]:text-muted-foreground",
@@ -111,7 +120,7 @@ const components = {
       {...props}
     />
   ),
-  th: ({ className, ...props }) => (
+  th: ({ className, ...props } : {className?: string}) => (
     <th
       className={cn(
         "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
@@ -120,7 +129,7 @@ const components = {
       {...props}
     />
   ),
-  td: ({ className, ...props }) => (
+  td: ({ className, ...props } : {className?: string}) => (
     <td
       className={cn(
         "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
@@ -129,7 +138,7 @@ const components = {
       {...props}
     />
   ),
-  pre: ({ className, ...props }) => (
+  pre: ({ className, ...props } : {className?: string}) => (
     <pre
       className={cn(
         "mb-4 mt-6 overflow-x-auto rounded-lg border bg-black py-4",
@@ -138,7 +147,7 @@ const components = {
       {...props}
     />
   ),
-  code: ({ className, ...props }) => (
+  code: ({ className, ...props } : {className?: string}) => (
     <code
       className={cn(
         "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
@@ -158,6 +167,18 @@ interface MdxProps {
 
 export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code)
+
+  // const components: MDXComponents = {
+  //   // h1: ({ className = "", ...props }: { className: string }) => <h1 className={className} {...props} />,
+  //   // h2: ({ className = "", ...props }: { className: string }) => <h2 className={className} {...props} />,
+  //   // h3: ({ className = "", ...props }: { className: string }) => <h3 className={className} {...props} />,
+  //   // h4: ({ className = "", ...props }: { className: string }) => <h4 className={className} {...props} />,
+  //   // Add other components here
+  //   // a: ({ className = "", ...props }: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => (
+  //   //   <a className={className} {...props} />
+  //   // ),
+  //   // Add other components here
+  // }
 
   return (
     <div className="mdx">
